6c4c16a40b9bbe076aed67d451d5d75070f9280f
ExnightMoon <ligengeek@126.com>
2018-06-23 15:24:33 +0800

    Test file changed

--- testtext.c
+++ testtext.c
@@ -1,15 +1,9 @@
 int main()
 {      
     int a = 5;
+    1t$&@#;
     double b = 1.3;
     float c = 3.14e-2;
-
-    
-    int 423434t = 7;
-    23
-    23
-    23
-    23
     string ch = "testtext";
     for(int i=0; i<=5; i++)  
     {
